8d7e722302a004ce854cbade6b100debb28d42f6
David H. <22520563+NewJerseyStyle@users.noreply.github.com>
2025-08-30 11:23:33 -0400

    Device constraint, fallback adam-atan2-pytorch
    https://github.com/lucidrains/adam-atan2-pytorch

--- pretrain.py
+++ pretrain.py
@@ -16,7 +16,7 @@ import coolname
 import hydra
 import pydantic
 from omegaconf import DictConfig
-from adam_atan2 import AdamATan2
+from adam_atan2_pytorch import AdamAtan2
 
 from puzzle_dataset import PuzzleDataset, PuzzleDatasetConfig, PuzzleDatasetMetadata
 from utils.functions import load_model_class, get_model_source_path
@@ -138,15 +138,14 @@ def create_model(config: PretrainConfig, train_metadata: PuzzleDatasetMetadata, 
         CastedSparseEmbeddingSignSGD_Distributed(
             model.model.puzzle_emb.buffers(),  # type: ignore
             
-            lr=0,  # Needs to be set by scheduler
+            lr=1e-8,  # Small initial lr, will be set by scheduler
             weight_decay=config.puzzle_emb_weight_decay,
 
             world_size=world_size
         ),
-        AdamATan2(
+        AdamAtan2(
             model.parameters(),
-
-            lr=0,  # Needs to be set by scheduler
+            lr=1e-8,  # Small initial lr, will be set by scheduler
             weight_decay=config.weight_decay,
             betas=(config.beta1, config.beta2)
         )
